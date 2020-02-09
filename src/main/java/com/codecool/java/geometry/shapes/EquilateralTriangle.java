16864360a4e806de544eebbe395aa17093099e3d
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-09 18:41:46 +0100

    Add EquilateralTriangle

--- src/main/java/com/codecool/java/geometry/shapes/EquilateralTriangle.java
+++ src/main/java/com/codecool/java/geometry/shapes/EquilateralTriangle.java
@@ -1,4 +1,30 @@
 package main.java.com.codecool.java.geometry.shapes;
 
 public class EquilateralTriangle extends Triangle {
+
+
+    public EquilateralTriangle(float a) {
+        super(a, a, a);
+        super.name = "Equilateral Triangle";
+        super.areaFormula = "sqrt(s(s-a)(s-b)(s-c))";
+        super.perimeterFormula = "a + b + c";
+    }
+
+    public double calculateArea(){
+        return super.calculateArea();
+    }
+
+    public double calculatePerimeter(){
+        return super.calculatePerimeter();
+    }
+
+    public String toString(){
+        return "EquilateralTriangle, a = " + super.getA();
+    }
+
+    public float getA(){
+        return super.getA();
+    }
+
+    
 }
