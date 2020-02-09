c74db33f3663950e3df83186beb05149768ec73b
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-09 18:41:28 +0100

    Add Circle

--- src/main/java/com/codecool/java/geometry/shapes/Circle.java
+++ src/main/java/com/codecool/java/geometry/shapes/Circle.java
@@ -1,4 +1,31 @@
 package main.java.com.codecool.java.geometry.shapes;
 
 public class Circle extends Shape {
+    private float r;
+
+
+    public Circle(float r) {
+        super("Circle", "pi*r^2", "2*pi*r");
+        checkIfArgsGreaterThanZero(r);
+        this.r = r;
+        // super.name = "Circle";
+        // super.areaFormula = "pi*r^2";
+        // super.perimeterFormula = "2*pi*r";
+    }
+
+    public double calculateArea(){
+        return Math.PI * Math.pow(this.r, 2);
+    }
+
+    public double calculatePerimeter(){
+        return 2 * Math.PI * r;
+    }
+
+    public String toString(){
+        return "Circle, r = " + this.r;
+    }
+
+    public float getR(){
+        return this.r;
+    }
 }
