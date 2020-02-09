f43579a7e92c75acdf8ab0269e79fd7d1d509321
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-09 18:16:13 +0100

    Add Triangle

--- src/main/java/com/codecool/java/geometry/shapes/Triangle.java
+++ src/main/java/com/codecool/java/geometry/shapes/Triangle.java
@@ -2,6 +2,40 @@
 package main.java.com.codecool.java.geometry.shapes;
 
 public class Triangle extends Shape {
+    private float a, b, c;
+
+    public Triangle(String name, String areaFormula, String perimeterFormula, float a, float b, float c) {
+        super("Triangle", "sqrt(s(s-a)(s-b)(s-c))", "a + b + c");
+        checkIfArgsGreaterThanZero(a,b,c);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
     
-    
+    public double calculateArea(){
+        double s = (a + b + c)/2;
+        double area = Math.sqrt(s*(s - a)*(s - b)*(s - c));
+        return area;
+    }
+
+    public double calculatePerimeter(){
+        double perimiter = a + b + c;
+        return perimiter;
+    }
+
+    public String toString(){
+        return "Triangle, a = " + this.a + ",b = " + this.b + ",c = " + this.c;
+    }
+
+    public float getA(){
+        return this.a;
+    }
+
+    public float getB(){
+        return this.b;
+    }
+
+    public float getC(){
+        return this.c;
+    }
 }
